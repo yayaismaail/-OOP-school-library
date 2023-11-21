454d2e9e1f472600fcf6aa5f9eab585290c30859
unknown <ismail halliru muhammad>
2023-11-21 11:21:31 +0100

    Update student.rb file

--- student.rb
+++ student.rb
@@ -1,12 +1,14 @@
-require './person'
+require_relative 'person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  attr_accessor :classroom
+
+  def initialize(name, age, parent_permission: true, classroom: nil)
+    super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\\(ツ)/¯'
   end
 end
