7b05216b0b4e2337c055bcc3403a655fa1c59b46
unknown <ismail halliru muhammad>
2023-11-21 11:22:00 +0100

    Update teacher.rb file

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,14 @@
-require './person'
+require_relative 'person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  attr_accessor :specialization
+
+  def initialize(name, age, specialization)
+    super(name: name, age: age)
     @specialization = specialization
   end
 
-  def can_use_services?
+  def can_use_library?
     true
   end
 end
