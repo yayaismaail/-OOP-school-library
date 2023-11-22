c9be850d34f314243feb08def06aba5f1d33802e
unknown <ismail halliru muhammad>
2023-11-22 11:44:43 +0100

    Implement a method called correct_name that will raise a NotImplementedError

--- person.rb
+++ person.rb
@@ -1,16 +1,24 @@
-class Person
+require './nameable'
+require './capitalize'
+require './trim'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @parent_permission = parent_permission
+    super()
   end
 
   attr_accessor :name, :age
   attr_reader :id
 
   def can_use_services?
-    of_age? || @parent_permission
+    of_age? || parent_permission ? true : false
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -19,3 +27,10 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
