93bbc37a01f9e1b43ba4ac8eab2df3ea026df5db
unknown <ismail halliru muhammad>
2023-11-22 12:55:20 +0100

    added a conditional statement to check the length before deciding whether to trim the name

--- trim.rb
+++ trim.rb
@@ -2,6 +2,10 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    @nameable.correct_name.slice! 0...10
+    if @nameable.correct_name.length > 10
+      @nameable.correct_name.slice(0, 10)
+    else
+      @nameable.correct_name
+    end
   end
 end
