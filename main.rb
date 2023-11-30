5e7f820168f28775e72510914c56eb8de9662bf1
unknown <ismail halliru muhammad>
2023-11-30 08:43:20 +0100

    Modify main.rb file

--- main.rb
+++ main.rb
@@ -1,12 +1,15 @@
 require 'date'
 require './app'
+
 def main
   app = App.new
   menu = Menu.new
+
   loop do
     menu.display_menu
     option = gets.chomp.to_i
     break if menu.process_option(option, app)
   end
 end
+
 main
