762308611371f09bfa08856f6aed298d6074aea6
Ismail Halliru Muhammad <ismailhm888@gmail.com>
2023-12-02 19:01:26 +0100

    Add solver_spec.rb file before adding solver.rb file

---
+++ spec/solver_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'solver'
+require 'rspec'
+
+describe Solver do
+  before(:each) do
+    @solver = Solver
+  end
+
+  it 'returns factorial of a number' do
+    expect(@solver.factorial(0)).to eq(1)
+    expect { @solver.factorial(-3) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
+  end
+
+  it 'reverses a given word' do
+    expect(@solver.reverse('hello')).to eq('olleh')
+    expect(@solver.reverse('world')).to eq('dlrow')
+  end
+
+  it 'applies fizzbuzz rules' do
+    expect(@solver.fizzbuzz(3)).to eq('fizz')
+    expect(@solver.fizzbuzz(5)).to eq('buzz')
+    expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    expect(@solver.fizzbuzz(7)).to eq('7')
+    expect { @solver.fizzbuzz('abc') }.to raise_error(ArgumentError, 'Input must be an integer')
+  end
+end
